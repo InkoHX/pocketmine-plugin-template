40460870cd554452f60601caa411ebf3c3aca2aa
InkoHX <inkohx@gmail.com>
2019-06-15 12:01:32 +0900

    Add files.

---
+++ src/plugin/Main.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace plugin;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase
+{
+    public function onLoad()
+    {
+        /** code */
+    }
+
+    public function onEnable()
+    {
+        /** code */
+    }
+
+    public function onDisable()
+    {
+        /** code */
+    }
+}
